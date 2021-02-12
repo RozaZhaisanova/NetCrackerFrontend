b77ba13b0d3b13802108ac746830c4c32148638f
Roza Zhaisanova <57352835+RozaZhaisanova@users.noreply.github.com>
2021-02-12 14:37:41 +0300

    container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Dialogs from "./components/Dialogs/Dialogs";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
@@ -14,12 +15,11 @@ const App = (props) => {
         <Navbar />
         <div className='app-wrapper-content'>
           <Route path="/dialogs"
-            render={() => <Dialogs store={props.store}  />} />
+            render={() => <DialogsContainer store={props.store} />} />
           <Route path="/profile"
             render={() => <Profile
-              profilePage={props.state.profilePage}
-              dispatch={props.dispatch}
-              />} />
+              store={props.store}
+            />} />
         </div>
 
       </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,35 +6,32 @@ import Post from "./Post/Post";
 
 const MyPosts = (props) => {
 
-let postElements = 
-props.posts.map(p =>       <Post message ={p.message} likesCount ={p.likesCount}/>
-);
-let newPostElement = React.createRef();
-let addPost=()=>{
-  //props.addPost();
-  props.dispatch(addPostActionCreator());
-}
-let onPostChange=()=>{
-  let text=newPostElement.current.value;
-  //props.updateNewPostText(text);
- // let action={type:'UPDATE-NEW-POST-TEXT',newText:text};
-  let action=updateNewPostTextActionCreator(text);
+  let postElements =
+    props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />
+    );
+  let newPostElement = React.createRef();
+  let onAddPost = () => {
+    props.addPost();
+  };
+  let onPostChange = () => {
+    let text = newPostElement.current.value;
+    props.updateNewPostText(text);
 
-  props.dispatch(action);
 
-}
-    return  (   
-    <div className = {s.postsBlock}> <h3>My posts</h3>
+  };
+  return (
+    <div className={s.postsBlock}> <h3>My posts</h3>
       <div><div>
-        <textarea onChange = {onPostChange} ref={newPostElement} value={props.newPostText}/></div>
-      <button onClick={addPost}>Add post</button>  
-      <button>Remove</button>  
+        <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} /></div>
+        <button onClick={onAddPost}>Add post</button>
+        <button>Remove</button>
 
       </div>
-      <div className = {s.posts}>
-      {postElements}
+      <div className={s.posts}>
+        {postElements}
 
       </div>
     </div>
-  )}
+  )
+}
 export default MyPosts;
